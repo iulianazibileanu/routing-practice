d366c4ceaeb05ec0a496aa210900fe4c02931e60
Iuliana-Zibileanu <iuliana_zibileanu@yahoo.com>
2024-02-18 16:10:35 +0200

    learned about queryParams and fragment

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,13 +18,17 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    this.route.params //if there will ever be any changes from within the component, this is a safer approach
+    this.paramSubscription = this.route.params //if there will ever be any changes from within the component, this is a safer approach
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name']
         }
       );
+  }
+
+  ngOnDestroy() {
+    this.paramSubscription.unsubscribe();
   }
 
 }
